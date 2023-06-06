a7dedb791e19b3987bd951b567277b21894ff713
sumit-bharat-mandlik <sumit-bharat.mandlik@hpe.com>
2023-06-06 10:48:24 +0530

    removed hidden

--- frontendscript.sh
+++ frontendscript.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 IMAGE_TAG="$1"
-template=`cat ".angular_deployment.txt" | sed "s/{{IMAGE_TAG}}/$IMAGE_TAG/g"`
+template=`cat "angular_deployment.txt" | sed "s/{{IMAGE_TAG}}/$IMAGE_TAG/g"`
 echo "$template" > deployment_angular.yaml
